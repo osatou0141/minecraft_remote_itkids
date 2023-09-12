b100fd6e941b92ba3074edb3d528f9c17e460ebb
osatou0141 <gbtikin@gmail.com>
2023-09-12 21:34:59 +0900

    ランダム羊毛色機能追加

--- itrobot/api_RS_osatou0141/sheep.py
+++ itrobot/api_RS_osatou0141/sheep.py
@@ -1,5 +1,6 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
+import random as rd
 
 mc = Minecraft.create(port=param.PORT_MC)
 
@@ -11,27 +12,27 @@ def leg(x, y, z):
     mc.setBlocks(x, y + 2, z, x + 7, y + 11, z, param.WHITE_TERRACOTTA)
     mc.setBlocks(x + 7, y + 2, z, x + 7, y + 11, z + 7, param.WHITE_TERRACOTTA)
     mc.setBlocks(x, y + 2, z + 7, x + 7, y + 11, z + 7, param.WHITE_TERRACOTTA)
-    mc.setBlocks(x - 1, y + 12, z - 1, x - 1, y + 21, z + 8, param.WHITE_WOOL)
-    mc.setBlocks(x + 8, y + 12, z - 1, x + 8, y + 21, z + 8, param.WHITE_WOOL)
-    mc.setBlocks(x - 1, y + 12, z - 1, x + 8, y + 21, z - 1, param.WHITE_WOOL)
-    mc.setBlocks(x - 1, y + 12, z + 8, x + 8, y + 21, z + 8, param.WHITE_WOOL)
+    mc.setBlocks(x - 1, y + 12, z - 1, x - 1, y + 21, z + 8, WOOL)
+    mc.setBlocks(x + 8, y + 12, z - 1, x + 8, y + 21, z + 8, WOOL)
+    mc.setBlocks(x - 1, y + 12, z - 1, x + 8, y + 21, z - 1, WOOL)
+    mc.setBlocks(x - 1, y + 12, z + 8, x + 8, y + 21, z + 8, WOOL)
 
 
 def body(x, y, z):
-    mc.setBlocks(x - 6, y + 22, z - 2, x + 33, y + 22, z + 21, param.WHITE_WOOL)
-    mc.setBlocks(x - 6, y + 39, z - 2, x + 33, y + 39, z + 21, param.WHITE_WOOL)
-    mc.setBlocks(x - 6, y + 22, z - 2, x + 33, y + 39, z - 2, param.WHITE_WOOL)
-    mc.setBlocks(x - 6, y + 22, z + 21, x + 33, y + 39, z + 21, param.WHITE_WOOL)
-    mc.setBlocks(x - 6, y + 22, z - 2, x - 6, y + 39, z + 21, param.WHITE_WOOL)
-    mc.setBlocks(x + 33, y + 22, z - 2, x + 33, y + 39, z + 21, param.WHITE_WOOL)
+    mc.setBlocks(x - 6, y + 22, z - 2, x + 33, y + 22, z + 21, WOOL)
+    mc.setBlocks(x - 6, y + 39, z - 2, x + 33, y + 39, z + 21, WOOL)
+    mc.setBlocks(x - 6, y + 22, z - 2, x + 33, y + 39, z - 2, WOOL)
+    mc.setBlocks(x - 6, y + 22, z + 21, x + 33, y + 39, z + 21, WOOL)
+    mc.setBlocks(x - 6, y + 22, z - 2, x - 6, y + 39, z + 21, WOOL)
+    mc.setBlocks(x + 33, y + 22, z - 2, x + 33, y + 39, z + 21, WOOL)
 
 
 def head(x, y, z):
-    mc.setBlocks(x + 1, y + 32, z + 3, x + 1, y + 45, z + 16, param.WHITE_WOOL)
-    mc.setBlocks(x + 1, y + 32, z + 3, x - 12, y + 45, z + 3, param.WHITE_WOOL)
-    mc.setBlocks(x + 1, y + 32, z + 16, x - 12, y + 45, z + 16, param.WHITE_WOOL)
-    mc.setBlocks(x + 1, y + 32, z + 3, x - 12, y + 32, z + 16, param.WHITE_WOOL)
-    mc.setBlocks(x + 1, y + 45, z + 3, x - 12, y + 45, z + 16, param.WHITE_WOOL)
+    mc.setBlocks(x + 1, y + 32, z + 3, x + 1, y + 45, z + 16, WOOL)
+    mc.setBlocks(x + 1, y + 32, z + 3, x - 12, y + 45, z + 3, WOOL)
+    mc.setBlocks(x + 1, y + 32, z + 16, x - 12, y + 45, z + 16, WOOL)
+    mc.setBlocks(x + 1, y + 32, z + 3, x - 12, y + 32, z + 16, WOOL)
+    mc.setBlocks(x + 1, y + 45, z + 3, x - 12, y + 45, z + 16, WOOL)
 
 
 def face(x, y, z):
@@ -57,4 +58,37 @@ def makesheep(x=0, y=63, z=0):
     mc.postToChat('finish')
 
 
+color = rd.randint(1, 16)
+if color == 1:
+    WOOL = param.WHITE_WOOL
+elif color == 2:
+    WOOL = param.ORANGE_WOOL
+elif color == 3:
+    WOOL = param.MAGENTA_WOOL
+elif color == 4:
+    WOOL = param.LIGHT_BLUE_WOOL
+elif color == 5:
+    WOOL = param.YELLOW_WOOL
+elif color == 6:
+    WOOL = param.LIME_WOOL
+elif color == 7:
+    WOOL = param.PINK_CONCRETE
+elif color == 8:
+    WOOL = param.GRAY_WOOL
+elif color == 9:
+    WOOL = param.LIGHT_GRAY_WOOL
+elif color == 10:
+    WOOL = param.CYAN_WOOL
+elif color == 11:
+    WOOL = param.PURPLE_WOOL
+elif color == 12:
+    WOOL = param.BLUE_WOOL
+elif color == 13:
+    WOOL = param.BROWN_WOOL
+elif color == 14:
+    WOOL = param.GREEN_WOOL
+elif color == 15:
+    WOOL = param.RED_WOOL
+else:
+    WOOL = param.BLACK_WOOL
 makesheep()
