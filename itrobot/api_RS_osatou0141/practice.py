c306b11506d0853bf6bb2967e1f8aa7119314436
osatou0141 <gbtikin@gmail.com>
2023-10-24 20:51:43 +0900

    sheep.py改良に伴う構文の修正

--- itrobot/api_RS_osatou0141/practice.py
+++ itrobot/api_RS_osatou0141/practice.py
@@ -4,4 +4,4 @@ import sheep as sh
 
 mc = Minecraft.create(port=param.PORT_MC)
 
-sh.spawn()
+sh.spawn(0, 63, 0)
--- itrobot/api_RS_osatou0141/reset.py
+++ itrobot/api_RS_osatou0141/reset.py
@@ -1,16 +1,10 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
+import sheep as sh
 
 mc = Minecraft.create(port=param.PORT_MC)
 
-
-def reset(x=0, y=63, z=0):
-    mc = Minecraft.create(port=param.PORT_MC)
-    mc.postToChat('reset field')
-    mc.setBlocks(x - 35, y, z - 35, x + 65, y + 100, z + 65, param.AIR)
-    mc.setBlocks(x - 35, y - 1, z - 35, x + 65, y - 1, z + 65, param.GRASS_BLOCK)
-    mc.postToChat('finish')
-
-
-if __name__ == "__main__":
-    reset()
+x = 0
+y = 63
+z = 0
+sh.reset(x, y, z)
