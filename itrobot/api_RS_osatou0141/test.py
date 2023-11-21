e4a0d9c86c09fa146c8e6ef7e8e1ec94320246c8
osatou0141 <gbtikin@gmail.com>
2023-11-21 21:38:40 +0900

    素材選択追加

--- itrobot/api_RS_osatou0141/test.py
+++ itrobot/api_RS_osatou0141/test.py
@@ -5,24 +5,33 @@ mc = Minecraft.create(port=param.PORT_MC)
 
 
 # 正方形
-def square(x, y, z, s):
+def square(x, y, z, s, mate):
+    if mate == 0:
+        mate = param.GOLD_BLOCK
     scale = s - 1
-    mc.setBlocks(x, y, z, x + scale, y + scale, z + scale, param.GOLD_BLOCK)
+    mc.setBlocks(x, y, z, x + scale, y + scale, z + scale, mate)
 
 
 # 長方形 #
-def rectangle(x, y, z, xs, zs, h):
+def rectangle(x, y, z, xs, zs, h, mate):
+    if mate == 0:
+        mate = param.IRON_BLOCK
     xsize = xs - 1
     zsize = zs - 1
     high = h - 1
-    mc.setBlocks(x, y, z, x + xsize, y + high, z + zsize, param.IRON_BLOCK)
+    mc.setBlocks(x, y, z, x + xsize, y + high, z + zsize, mate)
 
 
 # ひし形 #
-def rhombus(x, y, z, s, j, d):
+def rhombus(x, y, z, s, j, d, mate):
+    if mate == 0:
+        mate = param.DIAMOND_BLOCK
     s = d
     d = s
     h = 1
     n = h - 1
     length = j - 1
-    mc.setBlocks(x, y, z - n, x + length, y, z + n, param.DIAMOND_BLOCK)
+    mc.setBlocks(x, y, z - n, x + length, y, z + n, mate)
+
+
+rectangle(0, 63, 0, 5, 10, 7, 0)
